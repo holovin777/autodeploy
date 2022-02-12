6fe9166ab42211390fd0b965954bd40a9a55338c
Viktor Holovin <holovin@mail.com>
2022-02-12 17:10:14 +0100

    Fix non such file

--- autodeploy.py
+++ autodeploy.py
@@ -1,6 +1,7 @@
 import subprocess
+import os
 
-subprocess.run(["cd", "/home/admin/servicesapi"])
+os.chdir("/home/admin/servicesapi")
 subprocess.run(["source", "venv/bin/activate"])
 subprocess.run(["uvicorn", "main:app"])
 subprocess.run(["caddy", "reverse-proxy", "--from", "api.dottore.link", "--to", "localhost:8000"])
