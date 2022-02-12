d1f67049d3ab229da1aefa8cff29444ee8e325b8
Viktor Holovin <holovin@mail.com>
2022-02-12 23:57:12 +0100

    Testing2

--- autodeploy.py
+++ autodeploy.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 
 os.chdir("/home/admin/servicesapi")
-subprocess.run(["source", "venv/bin/activate"])
 subprocess.run(["uvicorn", "main:app"])
+subprocess.run(["caddy", "reverse-proxy", "--from", "api.dottore.link", "--to", "localhost:8000"])
