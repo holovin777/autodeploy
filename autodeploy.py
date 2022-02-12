77a4a92eaf8617733345b972fba97340f2b035ed
Viktor Holovin <holovin@mail.com>
2022-02-12 19:46:03 +0100

    https exists

--- autodeploy.py
+++ autodeploy.py
@@ -4,4 +4,3 @@ import os
 os.chdir("/home/admin/servicesapi")
 subprocess.run(["source", "venv/bin/activate"])
 subprocess.run(["uvicorn", "main:app"])
-subprocess.run(["caddy", "reverse-proxy", "--from", "api.dottore.link", "--to", "localhost:8000"])
