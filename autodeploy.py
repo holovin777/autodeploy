b488dcb422e834cc75fdcd3ab2e0509022899d00
Viktor Holovin <holovin@mail.com>
2022-02-12 23:58:31 +0100

    Add python -m

--- autodeploy.py
+++ autodeploy.py
@@ -2,5 +2,5 @@ import subprocess
 import os
 
 os.chdir("/home/admin/servicesapi")
-subprocess.run(["uvicorn", "main:app"])
+subprocess.run(["python", "-m", "uvicorn", "main:app"])
 subprocess.run(["caddy", "reverse-proxy", "--from", "api.dottore.link", "--to", "localhost:8000"])
